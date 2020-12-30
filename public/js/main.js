6be6f797b9164f73642209fb670d2c60f7d7fa10
DawiT <Dawid@Tararuj.be>
2020-12-30 15:18:24 +0100

    fin

---
+++ public/js/main.js
@@ -0,0 +1,51 @@
+let fruits = [`pomme`, `cerise`, `fraise`];
+
+//fruits = fruits.push("dawitue"); 
+
+fruits.forEach(function (cont){ console.log(cont) }); 
+
+console.log('-----');
+fr = fruits.push("adwiteu"); 
+
+fruits.forEach(function (cont){ console.log(cont) }); 
+
+console.log('-----');
+
+sup = fruits.shift("pomme"); //suprime au debut
+add = fruits.unshift("bananane");  // add au debut ; 
+// pop za supprimer le dernier elem ; 
+
+fruits.forEach(function (cont){ console.log(cont) }); 
+
+console.log('-----');
+
+
+
+
+let tab = ['banane', 'pomme', 'cerise'];
+
+change = "banane"; 
+change2 = "pomme de terre";
+
+//t2 = tab.indexOf(change); 
+t3 = tab[tab.indexOf(change)] = change2; 
+
+//t2 = tab.replace(tab.indexOf(change), change2);
+
+
+tab.forEach(function (cont){ console.log(cont) }); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
